3bde70227175320671c10cd2e8255ce639c07e72
Kahiro-M <0204.kahiro@gmail.com>
2023-02-13 13:09:25 +0900

    pwaでPOST

--- index.js
+++ index.js
@@ -29,3 +29,32 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 });
+
+
+$(function(){
+  $('input.in_url').on('change', function(){
+    // console.log('change in_url');
+    $('form').attr('action',$(this).val());
+  })
+  $('input.in_id_name').on('change', function(){
+    // console.log('change in_id_name');
+    $('input.in_id').attr('name',$(this).val());
+  })
+  $('input.in_id').on('change', function(){
+    console.log('change in_id');
+  })
+  $('input.in_pw_name').on('change', function(){
+    // console.log('change in_pw_name');
+    $('input.in_pw').attr('name',$(this).val());
+  })
+  $('input.in_pw').on('change', function(){
+    console.log('change in_pw');
+  })
+  $('input.in_hd_name').on('change', function(){
+    // console.log('change in_hd_name');
+    $('input.in_hd').attr('name',$(this).val());
+  })
+  $('input.in_hd').on('change', function(){
+    console.log('change in_hd');
+  })
+});
