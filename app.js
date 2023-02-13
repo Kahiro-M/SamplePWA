c468af10d10509f1bca326437594533e74def33b
Kahiro-M <0204.kahiro@gmail.com>
2023-02-13 10:56:51 +0900

    mdnに従う

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-//バナー表示をキャンセルし、代わりに表示するDOM要素を登録する関数
-//引数１：イベントを登録するHTMLElement
-function registerInstallAppEvent(elem){
-    //インストールバナー表示条件満足時のイベントを乗っ取る
-    window.addEventListener('beforeinstallprompt', function(event){
-      console.log("beforeinstallprompt: ", event);
-      event.preventDefault(); //バナー表示をキャンセル
-      elem.promptEvent = event; //eventを保持しておく
-      elem.style.display = "block"; //要素を表示する
-      return false;
-    });
-    //インストールダイアログの表示処理
-    function installApp() {
-      if(elem.promptEvent){
-        elem.promptEvent.prompt(); //ダイアログ表示
-        elem.promptEvent.userChoice.then(function(choice){
-          elem.style.display = "none";
-          elem.promptEvent = null; //一度しか使えないため後始末
-        });//end then
-      }
-    }//end installApp
-    //ダイアログ表示を行うイベントを追加
-    elem.addEventListener("click", installApp);
-  }//end registerInstallAppEvent
-  
-  //バナーの代わりに表示するボタンを登録する
-  registerInstallAppEvent(document.getElementById("InstallBtn"));
-  
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Chrome 67以前のバージョンでプロンプトが自動的に表示されないようにする
+  e.preventDefault();
+  // 後で発生させることができるように、イベントを隠しておく。
+  deferredPrompt = e;
+  // ホーム画面に内側へ追加できることをユーザーに通知する UI の更新
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // A2HS ボタンを表示するユーザーインターフェイスを非表示にします。
+    addBtn.style.display = 'none';
+    // プロンプトを表示
+    deferredPrompt.prompt();
+    // ユーザーがプロンプトに応答するのを待つ
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('ユーザーが A2HS プロンプトを受け入れました。');
+        } else {
+          console.log('ユーザーは A2HS のプロンプトを拒否しました。');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
