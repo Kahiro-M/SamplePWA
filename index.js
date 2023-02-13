c9ec58233fb6f8173935b7d0b648af4b71dd1fad
Kahiro-M <0204.kahiro@gmail.com>
2023-02-13 11:38:21 +0900

    fix

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
-const images = ['fox1', 'fox2', 'fox3', 'fox4'];
-const imgElem = document.querySelector('img');
-
-function randomValueFromArray(array) {
-  const randomNo = Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
-
-setInterval(() => {
-  const randomChoice = randomValueFromArray(images);
-  imgElem.src = `images/${randomChoice}.jpg`;
-}, 2000);
-
 // Register service worker to control making site work offline
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
-
-// Code to handle install prompt on desktop
-
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-// addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
