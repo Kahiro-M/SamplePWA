5c14b59d8741053bf1c0aa4dfe9c560b956d571a
Kahiro-M <0204.kahiro@gmail.com>
2023-02-13 10:21:39 +0900

    PWA修正

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
 self.addEventListener('fetch', function(e) {
   //空でOK
 })
+
+//インストール
+self.addEventListener('install', function(event) {
+	event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+			//console.log('Opened cache');
+			return cache.addAll(urlsToCache);
+      })
+	);
+});
