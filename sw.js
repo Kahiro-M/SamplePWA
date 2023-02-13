cfe5759ae3bf45625aa04f53e2f5208654220983
Kahiro-M <0204.kahiro@gmail.com>
2023-02-13 10:57:29 +0900

    mdnに従う

--- sw.js
+++ sw.js
@@ -2,11 +2,3 @@ self.addEventListener('fetch', function(e) {
   //空でOK
 })
 
-//インストール
-self.addEventListener('install', function(event) {
-	event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
-			//console.log('Opened cache');
-			return cache.addAll(urlsToCache);
-      })
-	);
-});
